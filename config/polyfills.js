d10cff4b6a7126bc8bda41a0e1f4dfba30f61835
Felipe Pereira <lf.pereirarj@gmail.com>
2018-10-08 15:37:27 -0300

    ajustes merge

--- config/polyfills.js
+++ config/polyfills.js
@@ -140,3 +140,12 @@ try {
   })();
 }
 
+// converte valores `float` em moeda
+Number.prototype.formatMoney = function(c = 2, d = ',', t = '.') {
+  var n = this;
+  c = isNaN(c = Math.abs(c)) ? 2 : c;
+  var s = n < 0 ? "-" : "";
+  var i = parseInt( n = Math.abs(+n || 0).toFixed(c) ) + "";
+  var j = ( j = i.length ) > 3 ? j % 3 : 0;
+  return s + ( j ? i.substr(0, j) + t : "" ) + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+}
